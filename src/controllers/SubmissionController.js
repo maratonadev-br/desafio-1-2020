21af1c41691d255787772719c6111a91352ce2a8
danitrod <danitrod@ibm.com>
2020-08-15 19:46:59 -0300

    Correct wrong email message

--- src/controllers/SubmissionController.js
+++ src/controllers/SubmissionController.js
@@ -32,10 +32,10 @@ module.exports = {
           case 502:
             msg = 'Você já atingiu o limite de submissões.';
             break;
-          case 504:
+          case 503:
             msg = `O usuário com e-mail ${process.env.USER_EMAIL} não foi encontrado. Para trocar o e-mail, apague a sua aplicação pela lista de recursos da IBM Cloud e realize o deploy novamente.`;
             break;
-          case 503:
+          case 504:
             msg =
               'As credenciais do modelo estão incorretas. Troque-as clicando no botão de configuração no canto superior direito.';
             break;
